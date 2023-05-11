253394378e06b4b6e557db2859b53b6132cc007f
[иgOradef <efimovsasha2007@gmail.com>
2023-05-11 23:22:22 +0300

    var5 for may

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 int main() {
 	int temp{-1};
 	int summary = 0;
+	int n{0};
 	std::cin >> temp;
 	while(temp != 0) {
-		if (temp % 6 == 0 && temp % 10 == 6) {
+		if (temp % 2 == 0) {
 
 			summary += temp;
 		}
+		n++;
+
 	    std::cin >> temp;
 	}
-	std::cout << summary;
+	std::cout << n << std::endl <<  summary;
 	return 0;
 }
