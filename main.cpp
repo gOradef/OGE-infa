a73b0be03d287a9d74bea5f2b9b19aedd95b35a0
[иgOradef <efimovsasha2007@gmail.com>
2023-05-11 23:29:36 +0300

    var 3 for may

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main() {
 	int temp{-1};
-	int max {3};
+	int max {2};
 	int n{0};
 	std::cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		std::cin >> temp;
-		if (temp % 10 == 3 && temp > max) {
+		if (temp % 10 == 2 && temp > max) {
 			max = temp;
 		}
 	}
