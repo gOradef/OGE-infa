513ece0ae823d85593af25a35b58e9bd472c98b2
[иgOradef <efimovsasha2007@gmail.com>
2023-05-12 12:24:13 +0300

    var11 for may

--- main.cpp
+++ main.cpp
@@ -2,19 +2,12 @@
 
 
 int main() {
-	int temp{-1};
-	int summary = 0;
-	int n{0};
-	std::cin >> temp;
-	while(temp != 0) {
-		if (temp % 2 == 0) {
-
-			summary += temp;
-		}
-		n++;
-
-	    std::cin >> temp;
-	}
-	std::cout << n << std::endl <<  summary;
+using namespace std;
+	int temp{-1}, summary;
+	while (temp != 0) {
+		if (temp % 5 == 0 || temp % 9 == 0) {summary ++;}
+		cin >> temp;
+	}	
+	cout << summary;
 	return 0;
 }
