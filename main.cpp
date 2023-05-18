ce9bc9e5fc6ee672e0360b0d904b76a75741a08e
[иgOradef <efimovsasha2007@gmail.com>
2023-05-18 17:20:50 +0300

    local 2nd exercise

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 int main() {
 using namespace std;
-	int n, temp, max {0};
+	int n, temp, max {30000};
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> temp;
-		if (temp % 5 == 0 && temp > max) {max = temp;}
+		if (temp % 10 == 4 && temp < max) {max = temp;}
 	}
 	cout << max;
 	return 0;
