7f60db684f11979b10aa1f32911776892b403581
[иgOradef <efimovsasha2007@gmail.com>
2023-05-18 17:17:39 +0300

    local 1st exercise

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main() {
 using namespace std;
-	int temp{-1}, num_counter;
-	double summary {0};
-	while (temp != 0) {
-		if (temp % 8 == 0) {summary += temp; num_counter++;}
+	int n, temp, max {0};
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
 		cin >> temp;
-	}	
-	summary = summary / num_counter;
-	if (summary == 0) {cout << "NO";} else cout << round(summary*10)/10;
+		if (temp % 5 == 0 && temp > max) {max = temp;}
+	}
+	cout << max;
 	return 0;
 }
